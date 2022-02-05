8ffff997124817dbffd12a4fc6d1d32086d6b822
leandroaur <leandro.aurelio@hotmail.com>
2022-02-05 09:24:50 -0300

    fixing fabio port conflict issue

--- launch-fabio-lb.sh
+++ launch-fabio-lb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 9999:9999 -p 9998:9998 -v $PWD/external-services/fabio.properties:/etc/fabio/fabio.properties fabiolb/fabio
+docker run -d -p 7999:9999 -p 7998:9998 -v $PWD/external-services/fabio.properties:/etc/fabio/fabio.properties fabiolb/fabio
